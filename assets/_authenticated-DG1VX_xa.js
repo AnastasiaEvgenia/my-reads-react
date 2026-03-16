7a734464a42dee63124b260f615747f037d86dea
Ευγενία <e.alexandri@ergologic.gr>
2026-03-16 23:58:17 +0200

    Updates

---
+++ assets/_authenticated-DG1VX_xa.js
@@ -0,0 +1,1 @@
+import{j as o,O as t}from"./index-CccuBPMc.js";import{A as p}from"./AppLayout-kS0VhMMW.js";import"./Modal-BfUUDWkV.js";const n=()=>o.jsx(p,{children:o.jsx(t,{})});export{n as component};
