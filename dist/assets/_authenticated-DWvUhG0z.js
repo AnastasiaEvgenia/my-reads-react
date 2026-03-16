3f75d3f7828cf5ddf44090caf44037d688025101
Ευγενία <e.alexandri@ergologic.gr>
2026-03-16 23:57:27 +0200

    build

--- dist/assets/_authenticated-DWvUhG0z.js
+++
@@ -1,1 +0,0 @@
-import{j as o,O as t}from"./index-Dd5JipS6.js";import{A as p}from"./AppLayout-wtCy08LJ.js";import"./Modal-BlqfOD8u.js";const n=()=>o.jsx(p,{children:o.jsx(t,{})});export{n as component};
