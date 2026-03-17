ae4327d17d8e9d8986700a5c262823508cfdf208
Ευγενία <e.alexandri@ergologic.gr>
2026-03-17 23:48:02 +0200

    build

--- dist/assets/_authenticated-DG1VX_xa.js
+++
@@ -1,1 +0,0 @@
-import{j as o,O as t}from"./index-CccuBPMc.js";import{A as p}from"./AppLayout-kS0VhMMW.js";import"./Modal-BfUUDWkV.js";const n=()=>o.jsx(p,{children:o.jsx(t,{})});export{n as component};
---
+++ dist/assets/_authenticated-NN8hyDZf.js
@@ -0,0 +1,1 @@
+import{j as o,O as t}from"./index-gMHRo7z1.js";import{A as p}from"./AppLayout-D6nRIxfQ.js";import"./Modal-CnpDxSjL.js";const n=()=>o.jsx(p,{children:o.jsx(t,{})});export{n as component};
